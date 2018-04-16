728f7640ba1b32caa4cc1d945e2932d34cf471ae
EddieChuang <az564123@gmail.com>
2018-04-16 14:08:44 +0800

    basic json file io
    append json object to json array hierarchically

---
+++ JSONOperator.java
@@ -0,0 +1,80 @@
+package jsonOperate;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JSONOperator {
+	
+	public static JSONArray append(JSONArray jsonArray, JSONObject jsonToAppend){
+		
+		if(jsonToAppend == null || jsonToAppend.length() == 0)
+			return jsonArray;
+		
+		try {
+				String cat  = jsonToAppend.getString("cat");
+				String text = jsonToAppend.getString("text"); 
+				for(int i = 0; i < jsonArray.length(); ++i){
+						JSONObject jsonObject = jsonArray.getJSONObject(i);
+						if(jsonObject.getString("cat").equals(cat) && jsonObject.getString("text").equals(text)){
+								JSONArray children = jsonObject.getJSONArray("children");
+								if(children.length() == 0){
+										jsonArray.put(jsonToAppend);
+										return jsonArray;
+								} else {
+									
+										jsonObject.put("children", append(children, jsonToAppend.getJSONArray("children").getJSONObject(0)));
+										jsonArray.put(i, jsonObject);
+										return jsonArray;
+								}
+						}
+				}
+		} catch (JSONException e) {
+				e.printStackTrace();
+		}
+
+		jsonArray.put(jsonToAppend);
+		return jsonArray;
+	}
+	
+public static JSONArray appendJstree(JSONArray jsonArray, JSONObject jsonToAppend){
+		
+		if(jsonToAppend == null || jsonToAppend.length() == 0)
+				return jsonArray;
+		try {
+				String cat  = jsonToAppend.getString("cat");
+				String text = jsonToAppend.getString("text"); 
+				for(int i = 0; i < jsonArray.length(); ++i){
+						JSONObject jsonObject = jsonArray.getJSONObject(i);
+						if(jsonObject.getString("cat").equals(cat) && jsonObject.getString("text").equals(text)){
+								JSONArray children = jsonObject.getJSONArray("children");
+								if(children.length() == 0){
+										return jsonArray;
+								} else {
+										jsonObject.put("children", appendJstree(children, jsonToAppend.getJSONArray("children").getJSONObject(0)));
+										jsonArray.put(i, jsonObject);
+										return jsonArray;
+								}
+						}
+				}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		jsonArray.put(jsonToAppend);
+		return jsonArray;
+	}
+
+	
+
+	public static void main(String[] args) {
+
+			try {
+				JSONObject json = new JSONObject("{a:1, a:2, a:3}");
+					
+				System.out.println(json);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+	}
+}
